2ba7e964156d00705723c3bce3441e34242b94bf
Aalay Kabariya <111956378+Aalay-kabariya@users.noreply.github.com>
2024-03-25 23:24:52 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import { Header, Home, SignUp, Login } from "./components"
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+// import {  } from "./components";
+import {Header, Home, Navbar, Login, SignUp, Contact, About, Shop } from "./components";
+
+// import Main from ".";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-      </div>
-    </Router>
+    <>
 
-    </div>
-  )
+      {/* <div className="Nav1">
+    
+        {/* <Navbar />
+        <Header /> */}
+        {/* <Main />      
+       
+        <Header /> */}
+    
+        {/* <Home /> */}
+        
+          
+          
+      
+
+      <Routes> {/* The Routes decides which component to show based on the current URL.*/}
+      <Route exact path="/" element={<div className="Nav1"><Navbar/> <Header /> <Home /></div>} />
+        <Route path="/shop" element={<><Navbar/><Shop /></>} />
+        <Route path="/contact" element={<><Navbar/><Contact/></>} />
+        <Route path="/about" element={<div className="about">< About/></div>} />
+        <Route path="/login" element={<><Login /></>} />
+        <Route path="/signup" element={<><SignUp /></>} />
+    </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+// import Home from './components/Home.jsx'
+// import Header from './components/Header.jsx'
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
